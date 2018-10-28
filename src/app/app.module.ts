487a80be07d808c16670aeeb47f39a7fbda08e25
MKA <matrixkali@wp.pl>
2018-10-28 07:40:50 +0100

    Angular update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppComponent} from './app.component';
-import {MainContainerComponent} from './main-container/main-container.component';
-import {ManageItemsComponent} from './manage-items/manage-items.component';
-import {ItemsListComponent} from './items-list/items-list.component';
-import {AppContainerComponent} from './app-container/app-container.component';
-import {AppRoutingModule} from './app-routing/app-routing.module';
-import {LoginPageComponent} from './login-page/login-page.component';
-import {AuthService} from './auth.service';
-import {AuthGuardService} from './auth-guard.service';
-import {FormsModule} from '@angular/forms';
+import { AppComponent } from './app.component';
+import { MainContainerComponent } from './main-container/main-container.component';
+import { ManageItemsComponent } from './manage-items/manage-items.component';
+import { ItemsListComponent } from './items-list/items-list.component';
+import { AppContainerComponent } from './app-container/app-container.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { AuthService } from './auth.service';
+import { AuthGuardService } from './auth-guard.service';
+import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
+  declarations : [
     AppComponent,
     LoginPageComponent,
     AppContainerComponent,
@@ -22,17 +22,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     ManageItemsComponent,
     ItemsListComponent
   ],
-  imports: [
+  imports : [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule
   ],
-  providers: [
+  providers : [
     AuthService,
     AuthGuardService
   ],
-  bootstrap: [AppComponent]
+  bootstrap : [ AppComponent ]
 })
 export class AppModule {
 }
