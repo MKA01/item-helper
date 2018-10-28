96037c57cca59690232d45d804261ce12f150887
MKA <matrixkali@wp.pl>
2018-10-28 19:46:37 +0100

    Update src/app/services/product.service.ts

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,44 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { Product } from '../product';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+
+@Injectable({
+  providedIn : 'root'
+})
+export class ProductService {
+  private _wasProductsListUpdated = new EventEmitter<void>();
+  private _products: Product[] = [];
+
+  constructor(private _httpClient: HttpClient) {
+
+  }
+
+  get wasProductsListUpdated(): EventEmitter<void> {
+    return this._wasProductsListUpdated;
+  }
+
+  get products(): Product[] {
+    return this._products.slice();
+  }
+
+  addProduct(product: Product) {
+    this._httpClient.post('http://localhost:3000/products', product)
+      .subscribe((response: HttpResponse<any>) => {
+        this._products.push(product);
+        this._wasProductsListUpdated.emit();
+      });
+  }
+
+  editProduct(product: Product, id: number) {
+    return this._httpClient.put(`http://localhost:3000/products/${id}`, product);
+  }
+
+  deleteProduct(id: number) {
+    return this._httpClient.delete(`http://localhost:3000/products/${id}`);
+  }
+
+  loadProducts() {
+    return this._httpClient.get('http://localhost:3000/products');
+  }
+
+}
